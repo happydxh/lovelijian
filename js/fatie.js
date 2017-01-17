caeef654359080ff1d27e21d774bde71739cf069
happydxh <1109747853@qq.com>
2017-01-17 20:30:23 +0800

    fatie update

--- js/fatie.js
+++ js/fatie.js
@@ -99,7 +99,6 @@ $(function(){
 								user:$.cookie('user')
 							},
 						success:function(texts){
-							//alert(texts)
 							
 							$(tiezilist[index]).find('#pinglunFace').attr('src',texts);
 						},
@@ -214,7 +213,6 @@ $(function(){
 						articleid:$(tiezilist[index]).find('#articleid').val()
 					},
 					success:function(response){
-						//alert(response)
 						var json = $.parseJSON(response);
 						var html = '';
 						var placeholder = ''
@@ -278,6 +276,7 @@ $(function(){
 					    	
 					    	//发表回复
 							$(this).find('#answer_Btn').on('click',function(){
+								
 								if( $(commentList[commentindex]).find('#answer_textarea').val() == '' ){
 									var tishi = $('#tishi');
 										tishi.show();
@@ -300,7 +299,10 @@ $(function(){
 									    //对发送的内容进行编码
 									    var answer_comment = encodeURIComponent(answer_textarea);
 									    
-									    var ansuser = $(commentList[commentindex]).find('.commentUser').text();
+									   // var ansuser = $(commentList[commentindex]).find('.commentUser').text();
+									    
+									    var ansuser =  $(this).parents('#answerForm').find('#answer_textarea').attr('placeholder').substr(3);
+									   
 									    
 										var answer_html = '';
 										answer_html += '<dt>'+
@@ -311,14 +313,12 @@ $(function(){
 											    				'</div>'+
 										    			'</dt>';
 										
+										
 				                        $(commentList[commentindex]).find('.answerol').prepend(answer_html);
-										$(commentList[commentindex]).find('#answerForm')[0].reset();
+				                        
+										//$(commentList[commentindex]).find('#answerForm')[0].reset();
 										
-										//评论数
-//										var count = $(tiezilist[index]).find('#count').text();
-//										count++
-//										$(tiezilist[index]).find('#count').text(count);
-										
+										//alert($(commentList[commentindex]).find('#commentid').val());
 										$.ajax({
 											type:"post",
 											url:"php/add_answer.php",
@@ -326,6 +326,11 @@ $(function(){
 												user:$.cookie('user'),
 												answer_comment:answer_comment,
 												commentid:$(commentList[commentindex]).find('#commentid').val()
+											},
+											success:function(response){
+												if(response == 1){
+													$(commentList[commentindex]).find('#answerForm')[0].reset();
+												}
 											},
 											async:true
 										});
@@ -358,7 +363,7 @@ $(function(){
 										//格式时间
 										var unix_time = get_unix_time(value.date);
 										var autotime = trantime(unix_time);
-										//alert(jiama)
+									
 										html+='<dt>'+
 									    				'<p class="ans_comment"><span class="placeholder answer_user">'+value.user+':</span> 回复 <span class="answer_user">'+value.ansuser+':</span>'+jiama+'</p>'+
 									    			    '<div class="answerBottom">'+
@@ -369,18 +374,77 @@ $(function(){
 									});
 									$(commentList[commentindex]).find('.answerol').append(html);
 									
+									
+									
 									//回复list
-									var huifuList = $(commentList[commentindex]).find('.answerol').children('dt');
+									var huifuList = $(commentList[commentindex]).find('.answerol').find('dt');
 									huifuList.each(function(i){
 										var huifuindex = i;
 										//显示隐藏回复标签
-										$(this).find('.huifu').on('click',function(){
+										
+										$(huifuList[huifuindex]).find('.huifu').on('click',function(){
 											var placeholder = $(this).parents('dt').find('.placeholder').text();
 											//console.log(placeholder)
-											$(commentList[commentindex]).find('#answerForm').toggle();
+											$(commentList[commentindex]).find('#answerForm').show();
 											$(commentList[commentindex]).find('#commentid').val($(this).attr('dataid'))
 											$(commentList[commentindex]).find('#answer_textarea').attr('placeholder','回复@'+placeholder);
-										})
+										})	
+											
+											//显示回复
+											$.ajax({
+												type:"post",
+												url:"php/show_answer.php",
+												data:{
+													commentid:$(this).find('.huifu').attr('dataid')
+												},
+												success:function(response){
+													var answer_json = $.parseJSON(response);
+													var html = '';
+													//alert(AnalyticEmotion(json[0]['comment']))
+													$.each(answer_json, function (index, value) {
+														//解码comment
+														var jiama = decodeURIComponent(value.comment);
+														//格式时间
+														var unix_time = get_unix_time(value.date);
+														var autotime = trantime(unix_time);
+														
+														html+='<dt>'+
+													    				'<p class="ans_comment"><span class="placeholder answer_user">'+value.user+':</span> 回复 <span class="answer_user">'+value.ansuser+':</span>'+jiama+'</p>'+
+													    			    '<div class="answerBottom">'+
+													    					'<time>'+autotime+'</time>'+
+													    					'<span class="huifu" dataid="'+value.id+'">回复</span>'+
+													    				'</div>'+
+												    			'</dt>';
+													});
+													$(commentList[commentindex]).find('.answerol').append(html);
+													
+													
+													//回复list
+													var huifuList = $(commentList[commentindex]).find('.answerol').children('dt');
+													huifuList.each(function(i){
+														var huifuindex = i;
+														//显示隐藏回复标签
+													
+														$(this).find('.huifu').on('click',function(){
+															
+														var placeholder = $(this).parents('dt').find('.placeholder').text();
+//															//console.log(placeholder)
+															$(commentList[commentindex]).find('#answerForm').show();
+															$(commentList[commentindex]).find('#commentid').val($(this).attr('dataid'))
+															$(commentList[commentindex]).find('#answer_textarea').attr('placeholder','回复@'+placeholder);
+															
+														    
+															
+															
+														})
+													})
+												},
+												async:true
+											});
+										    
+											
+											
+										
 										
 									})
 								},
