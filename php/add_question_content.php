7acf68fed9dbd3d09021eda5bc480ad6ab21c816
happydxh <1109747853@qq.com>
2017-02-02 21:34:54 +0800

    footer update

---
+++ php/add_question_content.php
@@ -0,0 +1,14 @@
+<?php
+	
+	
+	
+	require 'config.php';
+	
+	$query = "INSERT INTO lj_question (content, user, date) VALUES ('{$_POST['content']}', '{$_POST['user']}', NOW())";
+	
+	mysql_query($query) or die('新增失败！'.mysql_error());
+	
+	echo mysql_affected_rows();
+	
+	mysql_close();
+?>
